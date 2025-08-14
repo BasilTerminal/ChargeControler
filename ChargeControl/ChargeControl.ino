bb98b45eea9bf30161ef8d97d39d381e48bee1e7
Basil <terminal2014start@gmail.com>
2025-08-14 19:12:51 +0300

    last vers

--- ChargeControl/ChargeControl.ino
+++ ChargeControl/ChargeControl.ino
@@ -1,3 +1,5 @@
+ #include <stdlib.h>
+
 const int8_t QtyBatteries = 5;  // кількість портів девайса для підключення батарей. Портом я називаю
 
 const int8_t ConnectedBatteryPins[] = { 3, 5, 7, 10, 12 };        // піни які використовуються для вказання, який порт зараз підключений до зарядного присторою. LOW / HIGH
@@ -6,11 +8,9 @@ const int8_t BatteryVoltagePins[] = { A4, A3, A2, A1, A0 };       // піни з
 const int8_t ChargerCurrentPin = 5;                               // через цей пін можна считати силу струму, якою зарядник зараз заряджає підключену батарею. Значенння: 0 - 1023
 
 const int16_t VoltageLevelWhenBatteryPluged = 900;        // Рівень аналогового сигналу з піну батки, який означає, що батарея підключена до порту
-const int16_t CurrentLevelAtWhichContinueCharging = 300;  // Рівень аналогового сигналу з піну струму від зарядки, який означає, що заряд ще продовжуємо
+const int16_t CurrentLevelAtWhichContinueCharging = 40;  // Рівень аналогового сигналу з піну струму від зарядки, який означає, що заряд ще продовжуємо
 
 static void (*STATE)();
-
-int epsilon;
 
 
 void setup() {
@@ -20,57 +20,43 @@ void setup() {
     digitalWrite(ConnectedBatteryPins[i], LOW);
     pinMode(BatteryChargedIndicatorPins[i], OUTPUT);
     digitalWrite(BatteryChargedIndicatorPins[i], LOW);
-    //pinMode(BatteryVoltagePins[i], INPUT);
   }
-  //pinMode(ChargerCurrentPin, INPUT);
-  epsilon = CurrentOfCharging();
-
   STATE = &state01;
 }
 
 void loop() {
-  UpdateIndicationByInnerState();
+  updateIndicationByInnerState();
   (*STATE)();
-  delay(1200);
+  delay(200);
 }
 
 // Стани автомата
 
 // state01. В цьому стані шукуаємо на яку батарею перемкнути вихід зарядного пристрою. Якщо пішов струм через зярядку то переходимо в state02 інакше залишаємося в цьому стані.
 void state01() {
-
-  bool batteryWasFound = false;
   for (int8_t i = 0; i < QtyBatteries; i++) {
     if (batteryVoltage(i) > VoltageLevelWhenBatteryPluged && batteryChargedIndicator(i) == false) {
       setConnectedBattery(i);
-      batteryWasFound = true;
       break;
     }
   }
-  if (batteryWasFound == false) {
-    setConnectedBattery(-1);
-  }
-  // Serial.println(connectedBattery());
-  // setConnectedBattery(1);
-  // Serial.println(connectedBattery());
 
-
-  if (CurrentOfCharging() > CurrentLevelAtWhichContinueCharging) {
+  if (currentOfCharging() > 0) {
     STATE = &state02;
   }
 }
 
 // state02. В цьому стані чекаємо поки батарея заряджається, коли струм упав нижче критичного рівня то інидкуємо що батарея пройшла цикл зарядду і переходимо в state01
 void state02() {
-  if (CurrentOfCharging() < CurrentLevelAtWhichContinueCharging) {
+  if (currentOfCharging() <= CurrentLevelAtWhichContinueCharging) {
     int8_t battery = connectedBattery();
     setBatteryChargedIndicator(battery, true);
+    setConnectedBattery(-1);
     STATE = &state01;
   }
 }
 
-
-void UpdateIndicationByInnerState() {
+void updateIndicationByInnerState() {
   for (int8_t i = 0; i < QtyBatteries; i++) {
     int16_t voltage = batteryVoltage(i);
     Serial.print(voltage);
@@ -80,7 +66,7 @@ void UpdateIndicationByInnerState() {
     }
   }
   Serial.print(F("cur: "));
-  Serial.print(CurrentOfCharging());
+  Serial.print(currentOfCharging());
   Serial.print(F("\tconn: "));
   Serial.print(connectedBattery());
   Serial.print(F("\tstate: "));
@@ -100,42 +86,45 @@ int8_t connectedBattery() {
 }
 
 void setConnectedBattery(int8_t battery) {
-  // спочатку вимикаємо непотрібні, потім  включаємо потрібну
   if (battery >= QtyBatteries) {
     return;
   }
+  bool wasSwitch = false;
   for (byte i = 0; i < QtyBatteries; i++) {
+    if (digitalRead(ConnectedBatteryPins[i]) == HIGH) {
+      wasSwitch = true;
+    }
     digitalWrite(ConnectedBatteryPins[i], LOW);
   }
-
-  digitalWrite(ConnectedBatteryPins[battery], HIGH);
-
-  // for (int8_t i = 0; i < QtyBatteries; i++) {
-  //   if (i == battery) {
-  //     digitalWrite(ConnectedBatteryPins[i], HIGH);
-  //     break;
-  //   }
-  // }
-
-  // for (int8_t i = 0; i < QtyBatteries; i++) {
-  //   if (i != battery) {
-  //     digitalWrite(ConnectedBatteryPins[i], LOW);
-  //   }
-  // }
+  if (battery >= 0) {
+    if ( digitalRead(ConnectedBatteryPins[battery]) == LOW) {
+       wasSwitch = true;
+    }
+    digitalWrite(ConnectedBatteryPins[battery], HIGH);
+  }
+  if (wasSwitch) {
+    delay(3000);  // після будь-якого переключення батки треба зарядному дати час переключитися, підняти струм до потрібного рівня
+  }
 }
 
-int16_t CurrentOfCharging() {
-  int tok;
-  long sum = 0;
-  float average;
-  for (int i = 0; i < 1000; i++) {
-    sum = sum + analogRead(ChargerCurrentPin);
+ int comparation_two_int_for_qsort(const void *a, const void *b) {
+     return *(int*)a - *(int*)b;
+ }
+
+int16_t currentOfChargingRaw() {
+  return (analogRead(ChargerCurrentPin) - 512) * 48.2;
+}
+
+int16_t currentOfCharging() {
+  static int16_t epsilon = currentOfChargingRaw();
+  const int8_t N = 11;
+  int16_t mass[N];
+  for ( int8_t i = 0; i < N; i++) {
+    mass[i] = max(0, currentOfChargingRaw() - epsilon);
+    delay(5);
   }
-  average = (float)sum / 1000.0;
-  //Serial.print(average);
-  tok = int(((average - 512.0) * 48.2)) - epsilon;
-  //Serial.println(tok);
-  return tok;
+  qsort(mass, N, sizeof(int16_t), comparation_two_int_for_qsort);
+  return mass[N/2+1];
 }
 
 bool batteryChargedIndicator(int8_t battery) {
@@ -145,11 +134,11 @@ bool batteryChargedIndicator(int8_t battery) {
   return digitalRead(BatteryChargedIndicatorPins[battery]) == HIGH;
 }
 
-void setBatteryChargedIndicator(int8_t battery, bool STATE) {
+void setBatteryChargedIndicator(int8_t battery, bool flag) {
   if (battery < 0 || battery >= QtyBatteries) {
     return;
   }
-  digitalWrite(BatteryChargedIndicatorPins[battery], STATE ? HIGH : LOW);
+  digitalWrite(BatteryChargedIndicatorPins[battery], flag ? HIGH : LOW);
 }
 
 int16_t batteryVoltage(int8_t battery) {
